45e0fd862c69b64c9b6127d26e8ec4f7dc7586f4
Dan Pickett <dpickett@enlightsolutions.com>
2009-03-26 00:09:21 -0400

    more initial specs

---
+++ test/monkey_party/account_test.rb
@@ -0,0 +1,15 @@
+require "test_helper"
+
+class MonkeyParty::AccountTest < Test::Unit::TestCase
+  context "logging in" do
+    should "require a user name"
+    should "require a password"
+    should "return an api key"
+  end
+
+  context "adding an api key" do
+    should "require a user name"
+    should "require a password"
+    should "require an api key"
+  end
+end
