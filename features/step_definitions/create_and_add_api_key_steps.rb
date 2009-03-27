d9b7e3b9bfeb70ef9977a08c7979501a00c4ce47
Dan Pickett <dpickett@enlightsolutions.com>
2009-03-26 20:16:00 -0400

    fleshing out account feature

--- features/step_definitions/create_and_add_api_key_steps.rb
+++ features/step_definitions/create_and_add_api_key_steps.rb
@@ -1,5 +1,15 @@
 Given /^I have a valid mailchimp account$/ do
-  assert !@mailchimp_user.nil?
-  assert !@mailchimp_password.nil?
+  assert_not_nil configatron.mailchimp.user_name
+  assert_not_nil configatron.mailchimp.password
+
+  @mailchimp_user_name = configatron.mailchimp.user_name
+  @mailchimp_password  = configatron.mailchimp.password
 end
+
+When /^I attempt to login with the proper credentials$/ do
+  @account = MonkeyParty::Account.login(@mailchimp_user_name, 
+    @mailchimp_password)
+end
+
+
 
--- features/support/mailchimp/account.rb
+++ features/support/mailchimp/account.rb
@@ -1,4 +1,8 @@
 #load account settings from a yaml file
 
 require "configatron"
+require "ruby-debug"
+
+configatron.mailchimp.configure_from_yaml(
+  File.join(File.dirname(__FILE__), "account.yml"))
 
